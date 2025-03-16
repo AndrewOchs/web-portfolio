c67d054df452253c011e4c4bd53c66ac9c88db1a
AndrewOchs <tun78376@temple.edu>
2025-03-16 10:49:50 -0400

    bug fix

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,5 +1,5 @@
 import { motion } from 'framer-motion';
-import { default as React, default as React, useEffect, useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom'; // Use navigate instead of Link
 import './Hero.css';
 
