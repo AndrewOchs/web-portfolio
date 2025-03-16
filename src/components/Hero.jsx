05be27a66bc8491e6f70d21df1d9a60dbbd3ffc2
AndrewOchs <tun78376@temple.edu>
2025-03-16 10:48:02 -0400

    added import

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,5 +1,5 @@
 import { motion } from 'framer-motion';
-import React from 'react';
+import { default as React, default as React, useEffect, useState } from 'react';
 import { Link } from 'react-router-dom'; // Use navigate instead of Link
 import './Hero.css';
 
